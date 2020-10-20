ff8d8e431892efafd977dd660dc2c3d401bc8807
zhuyanwu <ywzhup@isoftstone.com>
2020-10-21 00:10:03 +0800

    11

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> list = null;
+        for(String a : list){
+            System.out.println(a);
+        }
+    }
+}
